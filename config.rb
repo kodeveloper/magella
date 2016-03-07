ce50a03f4d1cba1d109ef4e2f211da78cd55585f
Oğuzhan Demiröz <oguzhan@kodeveloper.co>
2016-03-07 14:11:35 +0200

    config.rb added and writed

---
+++ config.rb
@@ -0,0 +1,14 @@
+require 'susy'
+require 'breakpoint'
+
+project_type        = :stand_alone
+http_path           = "app/"
+sass_dir            = "app/assets/scss"
+css_dir             = "app/assets/css"
+images_dir          = "app/images"
+fonts_dir           = "app/assets/font"
+javascript_dir      = "app/assets/js"
+line_comments       = false
+preferred_syntax    = :scss
+output_style        = :compressed
+relative_assets     = true
